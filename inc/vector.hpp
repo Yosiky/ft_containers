6b5eadbedf4b28359e22d6c3d43de90afe6d0887
eestelle <yosiky@list.ru>
2023-01-04 16:42:48 +0300

    begin write vector

---
+++ inc/vector.hpp
@@ -0,0 +1,18 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+
+namespace ft {
+
+    template <typename T>
+    class vector {
+
+    public:
+
+        vector(void);
+        explicit vector(const vector<T> &obj);
+    };
+}
+
+
+#endif // VECTOR_HPP
