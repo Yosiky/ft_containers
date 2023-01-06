9c5f142cb354a3963c0a59a3181a871cefc59c21
eestelle <yosiky@list.ru>
2023-01-06 18:34:45 +0300

    add definition

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,9 +6,22 @@
 namespace ft {
     template<class T, class Allocator = std::allocator<T> > class vector {
     public:
+        vector();
+        explicit vector( const Allocator& alloc );
+        explicit vector( size_type count,
+                 const T& value = T(),
+                 const Allocator& alloc = Allocator());
+        template< class InputIt >
+vector( InputIt first, InputIt last,
+        const Allocator& alloc = Allocator() );
+        vector( const vector& other );
+        ~vector();
 
-        vector(void);
-        explicit vector(const vector<T> &obj);
+        vector& operator=( const vector& other );
+        void assign( size_type count, const T& value );
+        template< class InputIt >
+void assign( InputIt first, InputIt last );
+allocator_type get_allocator() const;
     };
 }
 
