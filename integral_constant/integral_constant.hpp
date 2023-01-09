9153f541d178c24b9746622183faca9401282e06
eestelle <yosiky@list.ru>
2023-01-09 20:33:31 +0300

    update(integral_constant): update class

--- integral_constant/integral_constant.hpp
+++ integral_constant/integral_constant.hpp
@@ -5,19 +5,15 @@
 
 namespace ft {
 
-    template <class T, T v = T()>
-    struct integral_constant {
-# if CPP11
-        constexpr static T value = v;
-# else
-        constexpr static T value;
-# endif
-    };
-
-# if PRECPP11
     template <class T, T v>
-    T integral_constant<T, v>::value = v;
-# endif
+    struct integral_constant {
+        typedef T                           value_type;
+        typedef ft::integral_constant<T, v> type;
+
+        constexpr static const T value = v;
+
+        constexpr operator value_type() const noexcept { return value; }
+    };
 
     typedef integral_constant<bool, true>   true_type;
     typedef integral_constant<bool, false>  false_type;
--- integral_constant/test.cpp
+++ integral_constant/test.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "integral_constant.hpp"
+// #include <type_traits>
 
 #define SHOW(value, res) \
     std::cout << #res << " == " << #value << " ? " << (res == value) << std::endl
 
+namespace on = ft;
+
 int main(void) {
-    SHOW((ft::integral_constant<bool, true>::value), true);
-    SHOW((ft::integral_constant<bool, false>::value), false);
+    SHOW((on::integral_constant<bool, true>::value), true);
+    SHOW((on::integral_constant<bool, false>::value), false);
 }
