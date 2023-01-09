e95dca4b11f3020ee7abd4f7e152fc8c940789ea
eestelle <yosiky@list.ru>
2023-01-09 20:56:03 +0300

    rearrange: new directory - equal

---
+++ equal/equal.hpp
@@ -0,0 +1,26 @@
+#ifndef EQUAL_HPP
+# define EQUAL_HPP
+
+namespace ft {
+
+    template<class InputIt1, class InputIt2>
+    constexpr bool equal(
+        InputIt1 first1,
+        InputIt1 last1,
+        InputIt2 first2 ) {
+        
+            
+    }
+
+    template<class InputIt1, class InputIt2, class BinaryPredicate>
+    constexpr bool equal(
+        InputIt1 first1,
+        InputIt1 last1,
+        InputIt2 first2,
+        BinaryPredicate p ) {
+
+    }
+
+}
+
+#endif
